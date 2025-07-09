0c5efa78418dd49dc42e8ad7b6634746d6dedf36
Samuel Parke <samueljparke@gmail.com>
2025-07-09 15:56:11 -0300

    dsc engine constructor

--- src/DSCEngine.sol
+++ src/DSCEngine.sol
@@ -5,6 +5,9 @@ pragma solidity ^0.8.20;
 import {DecentralisedStablecoin} from "../src/DecentralisedStablecoin.sol";
 
 contract DSCEngine {
+    // error
+    error DSCEngine__TokenAddressesAndPriceFeedAddressesDoNotMatch();
+
     address[] private s_collateralTokenAddresses;
 
     mapping(address user => mapping(address token => uint256 amount)) private s_userCollateralDepositted;
@@ -12,4 +15,16 @@ contract DSCEngine {
     mapping(address token => address priceFeed) private s_tokenPriceFeeds;
 
     DecentralisedStablecoin private immutable i_dsc;
+
+    constructor(address[] memory tokenAddresses, address[] memory priceFeedAddresses, address dsc) {
+        if (tokenAddresses.length != priceFeedAddresses.length) {
+            revert DSCEngine__TokenAddressesAndPriceFeedAddressesDoNotMatch();
+        }
+        // for the length of our tokenAddresses input, add the token address and equivalent price feed to the s_tokenPriceFeed mapping
+        for (uint256 i = 0; i < tokenAddresses.length; i++) {
+            s_tokenPriceFeeds[tokenAddresses[i]] = priceFeedAddresses[i];
+            s_collateralTokenAddresses.push(tokenAddresses[i]);
+        }
+        i_dsc = DecentralisedStablecoin(dsc);
+    }
 }
