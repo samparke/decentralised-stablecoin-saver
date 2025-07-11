1893cd215927f526af8aa0478e6aff3a98841017
Samuel Parke <samueljparke@gmail.com>
2025-07-11 07:51:38 -0300

    chainlink aggregator usage in getUsdValue

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 
 **This protocol builds upon my previous decentralised stablecoin project.**
 The purpose:
-- Rebuild the previous project with minimal assistance from tutorial.
+- Rebuild the previous project with minimal assistance from tutorials.
 - Build an additional layer, where users deposit collateral, receive a stablecoin, and can deposit to earn interest on the stablecoin.
--- src/DSCEngine.sol
+++ src/DSCEngine.sol
@@ -4,9 +4,10 @@ pragma solidity ^0.8.20;
 
 import {DecentralisedStablecoin} from "../src/DecentralisedStablecoin.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 contract DSCEngine {
-    // error
+    // ERRORS
     error DSCEngine__TokenAddressesAndPriceFeedAddressesDoNotMatch();
     error DSCEngine__TransferFailed();
     error DSCEngine__MintFailed();
@@ -14,14 +15,17 @@ contract DSCEngine {
     error DSCEngine_RedeemFailed();
     error DSCEngine__MustBeMoreThanZero();
 
-    address[] private s_collateralTokenAddresses;
+    uint256 private constant ADDITIONAL_FEED_PRECISION = 1e10;
+    uint256 private constant PRECISION = 1e18;
 
+    address[] private s_collateralTokenAddresses;
     mapping(address user => mapping(address token => uint256 amount)) private s_userCollateralDepositted;
     mapping(address user => uint256 dscMinted) private s_userDscMinted;
     mapping(address token => address priceFeed) private s_tokenPriceFeeds;
 
     DecentralisedStablecoin private immutable i_dsc;
 
+    // MODIFERS
     modifier moreThanZero(uint256 amount) {
         if (amount == 0) {
             revert DSCEngine__MustBeMoreThanZero();
@@ -41,6 +45,7 @@ contract DSCEngine {
         i_dsc = DecentralisedStablecoin(dsc);
     }
 
+    // DEPOSIT COLLATERAL, DEPOSIT AND MINT DSC, MINT DSC, BURN DSC, REDEEM COLLATERAL, REDEEM COLLATERAL FOR DSC
     function depositCollateral(address tokenCollateralAddress, uint256 amountToDeposit)
         public
         moreThanZero(amountToDeposit)
@@ -85,8 +90,36 @@ contract DSCEngine {
         redeemCollateral(tokenCollateralAddress, amountCollateral);
     }
 
-    // internal functions
+    // HEALTH FACTOR FUNCTIONS
 
+    function revertIfHealthFactorIsBroken() public {}
+
+    function calculateHealthFactor() public {}
+
+    function getAccountInformation(address user)
+        private
+        view
+        returns (uint256 totalDscMinted, uint256 collateralValueInUsd)
+    {
+        totalDscMinted = s_userDscMinted[user];
+        // collateralValueInUsd = _getAccountCollateralValue();
+    }
+
+    function getAccountCollateralValue(address user) private returns (uint256 collateralValue) {
+        for (uint256 i = 0; i < s_collateralTokenAddresses.length; i++) {
+            address token = s_collateralTokenAddresses[i];
+            uint256 amount = s_userCollateralDepositted[user][token];
+            // collateralValue = _getUsdValue(token, amount);
+        }
+    }
+
+    function getUsdValue(address token, uint256 amount) private returns (uint256) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(s_tokenPriceFeeds[token]);
+        (, int256 price,,,) = priceFeed.latestRoundData();
+        return ((uint256(price) * ADDITIONAL_FEED_PRECISION) * amount) / PRECISION;
+    }
+
+    // PRIVATE AND INTERNAL FUNCTIONS
     function _burnDsc(uint256 amountDscToBurn, address onBehalfOf, address dscFrom) private {
         s_userDscMinted[onBehalfOf] -= amountDscToBurn;
         // transfer dsc from msg.sender/liquidator (in burn function), to this address (equivalient to burn)
