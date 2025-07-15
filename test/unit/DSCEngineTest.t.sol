6a288ac2b0070daf4194d185914cad5b4f7b2564
Samuel Parke <samueljparke@gmail.com>
2025-07-15 09:12:29 -0300

    dscengine test setup

--- test/unit/DSCEngineTest.t.sol
+++ test/unit/DSCEngineTest.t.sol
@@ -7,6 +7,7 @@ import {DecentralisedStablecoin} from "../../src/DecentralisedStablecoin.sol";
 import {DSCEngine} from "../../src/DSCEngine.sol";
 import {DeployDSC} from "../../script/DeployDSC.s.sol";
 import {HelperConfig} from "../../script/HelperConfig.s.sol";
+import {ERC20Mock} from "../mocks/ERC20Mock.sol";
 
 contract DSCEngineTest is Test {
     DecentralisedStablecoin dsc;
@@ -16,9 +17,13 @@ contract DSCEngineTest is Test {
     address ethUsdPriceFeed;
     address btcUsdPriceFeed;
     address weth;
+    address public user = makeAddr("user");
+    uint256 STARTING_ERC20_BALANCE = 10 ether;
 
     function setUp() public {
         deployer = new DeployDSC();
         (dsc, dsce, config) = deployer.run();
+        (ethUsdPriceFeed, btcUsdPriceFeed, weth,,) = config.activeNetworkConfig();
+        ERC20Mock(weth).mint(user, STARTING_ERC20_BALANCE);
     }
 }
