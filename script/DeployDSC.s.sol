f50f155b71108f8674407e325bac1242088c0be7
Samuel Parke <samueljparke@gmail.com>
2025-07-15 09:02:29 -0300

    helper config, updated deployDSC

--- script/DeployDSC.s.sol
+++ script/DeployDSC.s.sol
@@ -4,12 +4,30 @@ pragma solidity ^0.8.20;
 
 import {Script} from "forge-std/Script.sol";
 import {DecentralisedStablecoin} from "../src/DecentralisedStablecoin.sol";
+import {HelperConfig} from "../script/HelperConfig.s.sol";
+import {DSCEngine} from "../src/DSCEngine.sol";
 
 contract DeployDSC is Script {
-    function run() external returns (DecentralisedStablecoin) {
-        vm.startBroadcast();
+    address[] public tokenAddresses;
+    address[] public priceFeedAddresses;
+
+    function run() external returns (DecentralisedStablecoin, DSCEngine, HelperConfig) {
+        HelperConfig config = new HelperConfig();
+        // gets the price feeds and token addresses from anvil chain
+        (address wethUsdPriceFeed, address wbtcUsdPriceFeed, address weth, address wbtc, uint256 deployerKey) =
+            config.activeNetworkConfig();
+        // we put these variables into the tokenAddresses and priceFeedAddresses to pass into DSCEngine deployment
+        tokenAddresses = [weth, wbtc];
+        priceFeedAddresses = [wethUsdPriceFeed, wbtcUsdPriceFeed];
+
+        // deployer key is our anvil private key
+        vm.startBroadcast(deployerKey);
         DecentralisedStablecoin dsc = new DecentralisedStablecoin();
+        // constructor variables
+        DSCEngine engine = new DSCEngine(tokenAddresses, priceFeedAddresses, address(dsc));
+        // we want the engine to be the address which can mint and burn etc
+        dsc.transferOwnership(address(engine));
         vm.stopBroadcast();
-        return dsc;
+        return (dsc, engine, config);
     }
 }
--- test/unit/DSCEngine.t.sol
+++ test/unit/DSCEngine.t.sol
@@ -6,13 +6,19 @@ import {Test} from "forge-std/Test.sol";
 import {DecentralisedStablecoin} from "../../src/DecentralisedStablecoin.sol";
 import {DSCEngine} from "../../src/DSCEngine.sol";
 import {DeployDSC} from "../../script/DeployDSC.s.sol";
+import {HelperConfig} from "../../script/HelperConfig.s.sol";
 
 contract DSCEngineTest is Test {
     DecentralisedStablecoin dsc;
     DeployDSC deployer;
     DSCEngine dsce;
+    HelperConfig config;
+    address ethUsdPriceFeed;
+    address btcUsdPriceFeed;
+    address weth;
 
     function setUp() public {
         deployer = new DeployDSC();
+        (dsc, dsce, config) = deployer.run();
     }
 }
